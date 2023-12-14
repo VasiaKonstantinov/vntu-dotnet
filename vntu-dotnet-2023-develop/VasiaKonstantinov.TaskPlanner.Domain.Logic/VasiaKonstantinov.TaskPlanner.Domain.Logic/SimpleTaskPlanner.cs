5853a0ecab465777ae05a92bbcda1b198a046783
Vasil Konstantinov <vasilok1310@gmail.com>
2023-12-14 10:24:43 +0200

    Commit

---
+++ vntu-dotnet-2023-develop/VasiaKonstantinov.TaskPlanner.Domain.Logic/VasiaKonstantinov.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VasiaKonstantinov.TaskPlanner.Domain.Models;
+
+namespace VasiaKonstantinov.TaskPlanner.Domain.Logic
+{
+    public class SimpleTaskPlanner
+    {
+        private readonly IWorkItemsRepository _workItemsRepository;
+
+        public SimpleTaskPlanner(IWorkItemsRepository workItemsRepository)
+        {
+            _workItemsRepository = workItemsRepository;
+        }
+
+        public WorkItem[] CreatePlan()
+        {
+            var items = _workItemsRepository.GetAll();
+            var itemsAsList = items.ToList();
+            itemsAsList.Sort(CompareWorkItems);
+            return itemsAsList.ToArray();
+        }
+
+        public WorkItem[] CreatePlanOld(WorkItem[] items)
+        {
+            var itemsAsList = items.ToList();
+            itemsAsList.Sort(CompareWorkItems);
+            return itemsAsList.ToArray();
+        }
+
+        private static int CompareWorkItems(WorkItem firstItem, WorkItem secondItem)
+        {
+            // Comparison logic here
+            // Sort by Priority (descending), then by DueDate (ascending), then by Title (alphabetical)
+            int priorityComparison = firstItem.Priority.CompareTo(secondItem.Priority);
+            if (priorityComparison != 0)
+                return -priorityComparison; // Descending order for Priority
+            int dueDateComparison = firstItem.DueDate.CompareTo(secondItem.DueDate);
+            if (dueDateComparison != 0)
+                return dueDateComparison; // Ascending order for DueDate
+            return string.Compare(firstItem.title, secondItem.title, StringComparison.Ordinal);
+        }
+    }
+}
